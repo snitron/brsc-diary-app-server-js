622087564225f88279dba359b50c3cfa1b4b9352
Nitron <nvar3@yandex.ru>
2019-03-19 22:41:28 +0300

    added parseMain.js

--- routes/getId.js
+++ routes/getId.js
@@ -1,29 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var Horseman = require('node-horseman');
-var horseman = new Horseman();
+
+const browser = require('zombie');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     var login = req.query.login;
     var password = req.query.password;
 
-    horseman
-        .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
-        .on('resourceError', function(err) {
-            console.log(err.message)
-        })
-        .open('https://elschool.ru/')
-        .type('#login', login)
-        .type('#password', password)
-        .wait(3000)
-        .click('#sub-btn')
-        .waitForNextPage({timeout: 30000})
-        .do(() => {
-            res.send(url);
-        })
-        .log()
-        .close();
+    browser.visit('https://elschool.ru', function () {
+        browser
+            .fill('#login', login)
+            .fill('#password', password)
+            .click('#stb-btn', function () {
+                res.send(browser.location.href);
+            });
+
+    });
 });
 
 module.exports = router;
