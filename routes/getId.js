e67f06d4cf202bc96c82f75f0f4b351fdca06d26
Nitron <nvar3@yandex.ru>
2019-03-19 20:45:40 +0300

    added parseMain.js

---
+++ routes/getId.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    var login = req.query.login;
+    var password = req.query.password;
+    var version = req.query.version;
+
+    const Nightmare = require('nightmare');
+    const nightmare = Nightmare({ show: true });
+
+    nightmare
+        .goto('https://elschool.ru/')
+        .evaluate(() => {
+            document.getElementById('login').value = login;
+            document.getElementById('password').value = password;
+            document.getElementById('sub-btn').click();
+        }).wait().evaluate(() => res.send(document.url));
+
+
+    //res.send(id)
+});
+
+module.exports = router;
+
+
+class UserModel{
+    child_model = Array();
+    id = "";
+}
--- routes/parseMain.js
+++ routes/parseMain.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     var id = req.query.id;
-
+    var login = req.query.login;
+    var password = req.query.password;
     res.send(id)
 });
 
