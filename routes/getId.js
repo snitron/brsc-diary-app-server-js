2f4cc7dfbb2bafd25b917b57b6e95f31a35b6c2a
Nitron <nvar3@yandex.ru>
2019-03-20 12:42:37 +0300

    added parseMain.js

--- routes/getId.js
+++ routes/getId.js
@@ -14,22 +14,44 @@ router.get('/', function (req, res, next) {
             await page.type('#login', login);
             await page.type('#password', password);
             await page.click('#sub-btn');
-
             await page.waitForNavigation().catch();
             await page.goto('https://elschool.ru/privateoffice');
+            await page.waitForNavigation().catch();
             await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'});
 
             const user = await page.evaluate(() => {
                 try {
-                    return $(document).ready(function () {
-                        var buttons = $('a.btn.btn-sm.btn-primary');
+                    var buttons = $('a.btn.btn-sm.btn-primary');
 
-                        if (buttons.length !== 0) {
+                    if (buttons.length !== 0) {
+                        //it is parent
+
+                        var child_ids = [];
+                        for (var i = 0; i < buttons.length; i++) {
+                            var elem = buttons[i].attr('href');
+                            data = String(elem).substring(String(elem).indexOf('?') + 1).split('&');
+                            child_ids.push({
+                                "rooId": data[0].substring(6),
+                                "instituteId": data[1].substring(12),
+                                "departmentId": data[2].substring(13),
+                                "userId": data[3].substring(8)
+                            });
+                        }
+                        user_id = null;
+
+                    } else {
+                        var child_ids = null;
+                        var user_id = $('p.col-lg-7.col-md-8.col-sm-8.col-6').first().text();
+
+
+                        /*
+                        var buttons = document.getElementsByClassName('a.btn.btn-sm.btn-primary');
+                        if (buttons.length !== 0)
                             //it is parent
 
                             var child_ids = [];
-                            for (var i = 0; i < buttons.length; i++) {
-                                var elem = buttons[i].attr('href');
+                            for(var i = 0; i < buttons.length; i++){
+                                var elem = buttons[i].getAttribute('href');
                                 data = String(elem).substring(String(elem).indexOf('?') + 1).split('&');
                                 child_ids.push({
                                     "rooId": data[0].substring(6),
@@ -39,36 +61,12 @@ router.get('/', function (req, res, next) {
                                 });
                             }
                             user_id = null;
-
                         } else {
                             var child_ids = null;
-                            var user_id = $('p.col-lg-7.col-md-8.col-sm-8.col-6').first().text();
-
-
-                            /*
-                            var buttons = document.getElementsByClassName('a.btn.btn-sm.btn-primary');
-                            if (buttons.length !== 0)
-                                //it is parent
-
-                                var child_ids = [];
-                                for(var i = 0; i < buttons.length; i++){
-                                    var elem = buttons[i].getAttribute('href');
-                                    data = String(elem).substring(String(elem).indexOf('?') + 1).split('&');
-                                    child_ids.push({
-                                        "rooId": data[0].substring(6),
-                                        "instituteId": data[1].substring(12),
-                                        "departmentId": data[2].substring(13),
-                                        "userId": data[3].substring(8)
-                                    });
-                                }
-                                user_id = null;
-                            } else {
-                                var child_ids = null;
-                                var user_id = document.getElementsByClassName('p.col-lg-7.col-md-8.col-sm-8.col-6')[0].text();
-                            }*/
-                        }
-                        return [child_ids, user_id];
-                    });
+                            var user_id = document.getElementsByClassName('p.col-lg-7.col-md-8.col-sm-8.col-6')[0].text();
+                        }*/
+                    }
+                    return [child_ids, user_id];
                 } catch
                     (e) {
                     return e.toString();
