01c2c18235fcc369a2d433527a9cbca681a20aa7
Nitron <nvar3@yandex.ru>
2019-03-20 17:51:47 +0300

    added parseMain.js

--- routes/parseMain.js
+++ routes/parseMain.js
@@ -13,7 +13,7 @@ router.get('/', function (req, res, next) {
            const puppeteer = require('puppeteer');
 
         (async () => {
-            const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+            const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: false});
             const page = await browser.newPage();
             await page.goto('https://elschool.ru/', {waitUntil: 'load', timeout: 0});
             await page.type('#login', login);
