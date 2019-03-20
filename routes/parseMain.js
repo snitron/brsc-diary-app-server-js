4bfb82d0d6e65de6100a254ead5843b96887cd13
Nitron <nvar3@yandex.ru>
2019-03-20 16:09:15 +0300

    added parseMain.js

--- routes/parseMain.js
+++ routes/parseMain.js
@@ -21,7 +21,7 @@ router.get('/', function (req, res, next) {
         await page.click('#sub-btn');
         await page.waitForNavigation().catch(() => console.log("catched"));
         await page.goto('https://elschool.ru/users/diaries/details?rooId=' + rooId
-            + "&instituteId=" + instituteId + "&departmentId=" + departmentId + "&pupilId=" + id, {waitUntil: 'load', timeout: 0});
+            + "&instituteId=" + instituteId + "&departmentId=" + departmentId + "&pupilId=" + id);
         await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'});
 
 
