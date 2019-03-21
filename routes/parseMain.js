b9c0e4ad3859b2e6a1e77d39b0268a142ad93dd6
Nitron <nvar3@yandex.ru>
2019-03-21 23:31:38 +0300

    added parseMain.js

--- routes/parseMain.js
+++ routes/parseMain.js
@@ -16,7 +16,7 @@ router.get('/', function (req, res, next) {
         const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const page = await browser.newPage();
        // await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36');
-        await page.emulate({'isMobile' : true, 'hasTouch' : true}, 'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36');
+        await page.emulate({viewport:{'isMobile' : true, 'hasTouch' : true}, userAgent :'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36'});
         await page.goto('https://elschool.ru/', {waitUntil: 'load', timeout: 0});
         await page.type('#login', login);
         await page.type('#password', password);
