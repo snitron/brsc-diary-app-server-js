8df8300ef441586c850834ac8710596230874815
Nitron <nvar3@yandex.ru>
2019-03-22 19:29:03 +0300

    added parseMain.js

--- routes/parseMain.js
+++ routes/parseMain.js
@@ -25,7 +25,7 @@ router.get('/', function (req, res, next) {
         await page.click('#sub-btn');
         await page.waitForNavigation().catch(() => console.log("catched"));
         await page.goto('https://elschool.ru/users/diaries/details?rooId=' + rooId
-            + "&instituteId=" + instituteId + "&departmentId=" + departmentId + "&pupilId=" + id + "&year=2019&week=12", {waitUntil: ['networkidle2', 'domcontentloaded']});
+            + '&instituteId=' + instituteId + '&departmentId=' + departmentId + '&pupilId=' + id + '&year=2019&week=12', {waitUntil: ['networkidle2', 'domcontentloaded']});
         //await page.waitForSelector('i.fa.fa-pencil-square-o').catch(() => console.log("catched"));
 
         await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'});
