5f73b013d57d7c17701ee5c3128dcfb5f36aa837
Nitron <nvar3@yandex.ru>
2019-03-22 22:42:04 +0300

    added parseMain.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var parseMainRouter = require('./routes/parseMain');
 var getIdRouter = require('./routes/getId');
+var getDiaryYears = require('./routes/getDiaryYears');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/parseMain', parseMainRouter);
 app.use('/getId', getIdRouter);
+app.use('/getDiaryYears', getDiaryYears);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/getDiaryYears.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    var id = req.query.id;
+    var login = req.query.login;
+    var password = req.query.password;
+    var rooId = req.query.rooid;
+    var instituteId = req.query.instituteId;
+    var departmentId = req.query.departmentId;
+
+    const puppeteer = require('puppeteer');
+
+    (async () => {
+        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36');
+        await page.goto('https://elschool.ru/', {waitUntil: 'load', timeout: 0});
+        await page.type('#login', login);
+        await page.type('#password', password);
+        await page.click('#sub-btn');
+        await page.waitForNavigation().catch(() => console.log("catched"));
+        await page.goto('https://elschool.ru/users/diaries/details?rooId=' + rooId
+            + '&instituteId=' + instituteId + '&departmentId=' + departmentId + '&pupilId=' + id + '&year=' + year + '&week=' + week, {waitUntil: ['networkidle2', 'domcontentloaded']});
+        await page.waitForSelector('#spinnerMessageSpan', {hidden: true});
+        await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'});
+
+        const mainData = await page.evaluate(() => {
+            try {
+                var data = $('option[model-department-id]');
+
+                var idS = [];
+                for (var i = 0; i < data.length; i++) {
+                    idS.push({
+                        'name': data.eq(i).text(),
+                        'departmentId': data.eq(i).text()
+                    })
+                }
+
+                return idS;
+            } catch (e) {
+                return e.toString();
+            }
+        });
+        res.send(JSON.stringify(mainData));
+
+        await browser.close();
+    })();
+});
+
+module.exports = router;
