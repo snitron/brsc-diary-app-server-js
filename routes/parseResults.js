97f0f585a75a2151435778ae8fcbbe7e788eef32
Nitron <nvar3@yandex.ru>
2019-03-23 18:01:30 +0300

    added parseMain.js

--- routes/parseResults.js
+++ routes/parseResults.js
@@ -32,77 +32,77 @@ router.get('/', function (req, res, next) {
 
                 var data = [];
 
-                if ($('#result-marks-table').eq(0).find('thead').eq(0).find('tr').eq(0).find('td').length === 10)
-                for (var i = 0; i < trS.length; i++) {
-                    var lessonResults = {};
+                if ($('#result-marks-table').eq(0).find('thead').eq(0).find('tr').eq(0).find('td').length === 10) {
+                    for (var i = 0; i < trS.length; i++) {
+                        var lessonResults = {};
 
-                    lessonResults.lesson = '';
-                    lessonResults.m1 = '';
-                    lessonResults.m2 = '';
-                    lessonResults.m3 = '';
-                    lessonResults.m4 = '';
-                    lessonResults.y = '';
-                    lessonResults.res = '';
-                    lessonResults.test = '';
-                    lessonResults.isHalfYear = false;
+                        lessonResults.lesson = '';
+                        lessonResults.m1 = '';
+                        lessonResults.m2 = '';
+                        lessonResults.m3 = '';
+                        lessonResults.m4 = '';
+                        lessonResults.y = '';
+                        lessonResults.res = '';
+                        lessonResults.test = '';
+                        lessonResults.isHalfYear = false;
 
-                    var tdS = trS.eq(i).find('td');
+                        var tdS = trS.eq(i).find('td');
 
 
-                    if (tdS.eq(1).text().trim() === '' &&
-                        tdS.eq(2).text().trim() === '' &&
-                        tdS.eq(3).text().trim() === '' &&
-                        tdS.eq(4).text().trim() === '' &&
-                        (tdS.eq(5).text().trim() !== '' || tdS.eq(6).text().trim()))
-                        lessonResults.isHalfYear = true;
+                        if (tdS.eq(1).text().trim() === '' &&
+                            tdS.eq(2).text().trim() === '' &&
+                            tdS.eq(3).text().trim() === '' &&
+                            tdS.eq(4).text().trim() === '' &&
+                            (tdS.eq(5).text().trim() !== '' || tdS.eq(6).text().trim()))
+                            lessonResults.isHalfYear = true;
 
-                    lessonResults.lesson = tdS.eq(0).text().trim();
+                        lessonResults.lesson = tdS.eq(0).text().trim();
 
-                    if (lessonResults.isHalfYear) {
-                        lessonResults.m1 = tdS.eq(5).text().trim();
-                        lessonResults.m2 = tdS.eq(6).text().trim();
-                    } else {
+                        if (lessonResults.isHalfYear) {
+                            lessonResults.m1 = tdS.eq(5).text().trim();
+                            lessonResults.m2 = tdS.eq(6).text().trim();
+                        } else {
+                            lessonResults.m1 = tdS.eq(1).text().trim();
+                            lessonResults.m2 = tdS.eq(2).text().trim();
+                            lessonResults.m3 = tdS.eq(3).text().trim();
+                            lessonResults.m4 = tdS.eq(4).text().trim();
+                        }
+
+                        lessonResults.y = tdS.eq(7).text().trim();
+                        lessonResults.test = tdS.eq(8).text().trim();
+                        lessonResults.res = tdS.eq(9).text().trim();
+
+                        data.push(lessonResults);
+                    }
+                } else {
+                    for (var i = 0; i < trS.length; i++) {
+                        var lessonResults = {};
+
+                        lessonResults.lesson = '';
+                        lessonResults.m1 = '';
+                        lessonResults.m2 = '';
+                        lessonResults.m3 = '';
+                        lessonResults.m4 = '';
+                        lessonResults.y = '';
+                        lessonResults.res = '';
+                        lessonResults.test = '';
+                        lessonResults.isHalfYear = false;
+
+                        var tdS = trS.eq(i).find('td');
+
+                        lessonResults.lesson = tdS.eq(0).text().trim();
+
                         lessonResults.m1 = tdS.eq(1).text().trim();
                         lessonResults.m2 = tdS.eq(2).text().trim();
                         lessonResults.m3 = tdS.eq(3).text().trim();
                         lessonResults.m4 = tdS.eq(4).text().trim();
+
+                        lessonResults.test = tdS.eq(5).text().trim();
+                        lessonResults.res = tdS.eq(6).text().trim();
+
+                        data.push(lessonResults);
                     }
-
-                    lessonResults.y = tdS.eq(7).text().trim();
-                    lessonResults.test = tdS.eq(8).text().trim();
-                    lessonResults.res = tdS.eq(9).text().trim();
-
-                    data.push(lessonResults);
                 }
-            else
-                for (var i = 0; i < trS.length; i++) {
-                    var lessonResults = {};
-
-                    lessonResults.lesson = '';
-                    lessonResults.m1 = '';
-                    lessonResults.m2 = '';
-                    lessonResults.m3 = '';
-                    lessonResults.m4 = '';
-                    lessonResults.y = '';
-                    lessonResults.res = '';
-                    lessonResults.test = '';
-                    lessonResults.isHalfYear = false;
-
-                    var tdS = trS.eq(i).find('td');
-
-                    lessonResults.lesson = tdS.eq(0).text().trim();
-
-                    lessonResults.m1 = tdS.eq(1).text().trim();
-                    lessonResults.m2 = tdS.eq(2).text().trim();
-                    lessonResults.m3 = tdS.eq(3).text().trim();
-                    lessonResults.m4 = tdS.eq(4).text().trim();
-
-                    lessonResults.test = tdS.eq(5).text().trim();
-                    lessonResults.res = tdS.eq(6).text().trim();
-
-                    data.push(lessonResults);
-                }
-
                 return data;
             } catch (e) {
                 return e.toString()
